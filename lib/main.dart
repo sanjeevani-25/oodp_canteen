b6dbe3e24ea6cf574ba01555d614284357d28f1d
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-06-09 15:40:24 +0530

    main-dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-import './screens/login-page.dart';
-
+import 'ui/OnboardingScreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      title: 'Onboarding Screen',
+      home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Login Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
     );
   }
 }
- 
-
